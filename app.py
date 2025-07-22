62b9b51b409c194ba1450015c44ab2570967bebd
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-07-22 16:47:08 +0530

    made a change in app.py

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, request, jsonify 
 
-app=Flaks(__name__)
+app=Flask(__name__)
 
 #home
 @app.route('/')
 def home():
     return "welcome to the Flask"
 
-#get route
+#GET route
+@app.route('api/data', methods=['GET'])
+def get_data():
+    return jsonify({
+        "message": "Hello This is me and my data",
+        "status": "success"
+    })
+
+#POST route 
+@app.route("/api/submit",method=['POST'])
+def submit_data():
+    data = request.get_json()
+    if not data:
+        return jsonify({
+            "received_data":data,
+            "message":"Data received successfully"
+        })
+
+if __name__=='__main__':
+    app.run(debug=True)
